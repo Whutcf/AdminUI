1948e067fb045517bf0261b947b2b4c0f2220372
Whutcf <18202784536@163.com>
2019-06-21 21:19:48 +0800

    使用lambok的@Slf4j注解

--- AdminUI/src/main/java/com/smic/cf/controller/PageController.java
+++ AdminUI/src/main/java/com/smic/cf/controller/PageController.java
@@ -1,49 +1,47 @@
 package com.smic.cf.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class PageController {
 
-	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
-	
 	@GetMapping("/frame")
 	public String index() {
-		logger.info("进入主页面");
+		log.info("进入主页面");
 		return "frame";
 	}
-	
-	
+
 	@GetMapping("/toFrame")
 	public String toIndex() {
-		logger.info("进入主页面");
+		log.info("进入主页面");
 		return "frame";
 	}
-	
+
 	@GetMapping("tgls/toQdAPI")
 	public String toQdAPI() {
-		logger.info("进入前端API，内有js用法提示内容");
+		log.info("进入前端API，内有js用法提示内容");
 		return "tgls/qdAPI";
 	}
-	
+
 	@GetMapping("tgls/toIconfont")
 	public String toIconfont() {
-		logger.info("进入添加分管页面");
+		log.info("进入添加分管页面");
 		return "tgls/iconfont";
 	}
 
 	@GetMapping("tgls/agent/toAgent_add")
 	public String toAgentAdd() {
-		logger.info("进入添加分管页面");
+		log.info("进入添加分管页面");
 		return "tgls/agent/agent_add";
 	}
-	
+
 	@GetMapping("tgls/agent/toAgent_list")
 	public String toAgentList() {
-		logger.info("进入分管列表页面");
+		log.info("进入分管列表页面");
 		return "tgls/agent/agent_list";
 	}
 }
--- AdminUI/src/main/java/com/smic/cf/controller/UserController.java
+++ AdminUI/src/main/java/com/smic/cf/controller/UserController.java
@@ -1,25 +1,24 @@
 package com.smic.cf.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class UserController {
-
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@RequestMapping("tgls/toModifyPassword")
 	public String modifyPassword() {
-		logger.info("进入修改密码页面！");
+		log.info("进入修改密码页面！");
 		return "tgls/modify_password";
 	}
 	
 	@GetMapping("/toLogin")
 	public String login() {
-		logger.info("登录页面");
+		log.info("登录页面");
 		return "login1";
 	}
 
