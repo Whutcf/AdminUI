d039ecbdf29cdc8d0a055342a477b3931a065cd7
Whutcf <1967380642@qq.com>
2020-01-16 21:14:02 +0800

    旧代码保存！

--- AdminUI/src/main/java/com/smic/cf/mapper/master/RoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.smic.cf.mapper.master;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.smic.cf.domain.Role;
-
-public interface RoleMapper extends BaseMapper<Role> {
-
-
-}
